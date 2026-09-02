3ed4f4913073e775362100b39048232c6a69a65b
Danila Shubin <schub.danil@yandex.ru>
2026-09-02 14:23:25 +0300

    Prepared code added for lec-3

---
+++ class-02/factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int factorial(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+
+int main(void) {
+
+    printf("%d\n", factorial(2));
+
+
+    return 0;
+}
